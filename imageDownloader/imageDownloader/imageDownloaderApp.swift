5ff582c7496ae8b3705df9433bde149366f1e0b2
Volnei Foss <volnei.foss@townsq.com.br>
2025-01-02 22:11:31 -0300

    initial project

---
+++ imageDownloader/imageDownloader/imageDownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  imageDownloaderApp.swift
+//  imageDownloader
+//
+//  Created by Volnei Foss on 02/01/25.
+//
+
+import SwiftUI
+
+@main
+struct imageDownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
